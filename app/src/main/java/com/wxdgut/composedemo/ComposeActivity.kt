7db166c551290f430adea1eef0f7ac1732ccc899
luyao <hello_luyao@163.com>
2023-11-10 16:06:22 +0800

    使用布局组件：Image基本使用

--- app/src/main/java/com/wxdgut/composedemo/ComposeActivity.kt
+++ app/src/main/java/com/wxdgut/composedemo/ComposeActivity.kt
@@ -3,7 +3,9 @@ package com.wxdgut.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -11,12 +13,14 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
@@ -39,8 +43,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -172,6 +181,60 @@ class ComposeActivity : ComponentActivity() {
                     它还包含了一些基本的内部内容，例如本文。
                     您已按下浮动动作按钮 $presses 次。
                 """.trimIndent(),
+                )
+                ShowImage()
+            }
+        }
+    }
+
+    @Composable
+    fun ShowImage() {
+        Row(
+            Modifier.background(Pink80),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            //定义图片Modifier
+            val imageModifier = Modifier
+                .size(90.dp, 120.dp)
+                .border(BorderStroke(2.dp, Red))
+                .background(Purple80)
+                .clip( RoundedCornerShape(50.dp, 50.dp, 0.dp, 0.dp))
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.img_android_horizontal),
+                    contentDescription = "这是备注，可填null",
+                    modifier = imageModifier
+                )
+            }
+            Column(
+                Modifier.padding(start = 10.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_android_vertical),
+                    contentDescription = null,
+                    modifier = imageModifier
+                )
+            }
+            Column(
+                Modifier.padding(start = 10.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.img_android_transparent_bg),
+                    contentDescription = "原来的图片是正方形单一蓝色，改为单一绿色，拉伸图片，填充宽高",
+                    modifier = imageModifier,
+                    contentScale = ContentScale.FillBounds,
+                    colorFilter = ColorFilter.tint(color = Color.Green, BlendMode.SrcAtop)
+                )
+            }
+            Column(
+                Modifier.padding(start = 10.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.img_android_horizontal2),
+                    contentDescription = "None:截取图片中心放大。缩放为Fit时，clip无效",
+                    modifier = imageModifier,
+                    contentScale = ContentScale.None
                 )
             }
         }
